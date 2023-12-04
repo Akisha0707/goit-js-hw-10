323862ff2c59a223473d5e1f345d6df1323ccada
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-12-04 15:35:00 +0200

    files js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] = 'твій ключ';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_gqCDtXROVBYQEPNW3vLvgVi7ZXwNuvMNOnquLQrpKSOZsOhw5jDghmYIy1yEpyZw';
+
+console.log(
+  axios.get('https://api.thecatapi.com/v1/images/search?breed_ids=beng,abys')
+);
+
+// fetchCatByBreed(breedId) {
+//     return
+// };
+// select.breed-select
